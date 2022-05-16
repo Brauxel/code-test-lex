7575248b8f257217333394ada3812c1794e89222
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-05-17 00:55:01 +1000

    [Aakash Bhatia]-[moved react app example to new page]

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,14 @@
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MoviesListing } from "./components/pages/MoviesListing";
+import ReactAppExample from "./components/pages/ReactAppExample";
 
-function App() {
+export const App = () => {
   return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Counter />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <span>
-            <span>Learn </span>
-            <a
-              className="App-link"
-              href="https://reactjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Redux
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux-toolkit.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Redux Toolkit
-            </a>
-            ,<span> and </span>
-            <a
-              className="App-link"
-              href="https://react-redux.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React Redux
-            </a>
-          </span>
-        </header>
-      </div>
-
-      <section>
-        <MoviesListing />
-      </section>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MoviesListing />} />
+        <Route path="/react-example" element={<ReactAppExample />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
-
-export default App;
+};
